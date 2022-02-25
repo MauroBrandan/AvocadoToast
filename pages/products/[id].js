d7cc7ac4dfd658041cf3bbc160e17d2c989fe10d
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-02-25 19:57:48 -0300

    Add a loader

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { Segment, Dimmer, Loader } from 'semantic-ui-react'
 import { ProductDetail } from '../../components/ProductDetail'
 
 function Product() {
@@ -21,7 +22,13 @@ function Product() {
 	}, [id])
 
 	if (loading) {
-		return <p>Loading...</p>
+		return (
+			<Segment style={{ height: '100%', border: 'none', boxShadow: 'none' }}>
+				<Dimmer active inverted>
+					<Loader size='large'>Loading</Loader>
+				</Dimmer>
+			</Segment>
+		)
 	}
 
 	return (
