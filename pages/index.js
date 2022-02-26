45dcc3fb7bc32aff70209d5a6d68fd017b46cd4a
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-02-25 21:01:55 -0300

    Homepage goes static with getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,21 @@ import React, { useState, useEffect } from 'react'
 import { CallToAction } from '../components/CallToAction'
 import { ProductsList } from '../components/ProductsList'
 
-function Home() {
-	const [products, setProducts] = useState([])
+export const getStaticProps = async () => {
+	const response = await fetch('https://avocado-toast.vercel.app/api/avo')
+	const { data } = await response.json()
+	return {
+		props: {
+			productList: data,
+		},
+	}
+}
 
-	useEffect(() => {
-		window
-			.fetch('/api/avo')
-			.then((response) => response.json())
-			.then(({ data }) => {
-				setProducts(data)
-			})
-	}, [])
-
+function Home({ productList }) {
 	return (
 		<>
 			<CallToAction url='/about'>5 Surprising Facts About Avocados</CallToAction>
-			<ProductsList products={products} />
+			<ProductsList products={productList} />
 		</>
 	)
 }
